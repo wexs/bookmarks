6fd19e9653fdbe988e01e12d3fefc5a2122f6f4b
wexs <wexstm@gmail.com>
2024-06-22 00:47:20 +0800

    添加链接

--- popup.js
+++ popup.js
@@ -147,8 +147,12 @@ function convertToHtml(nodes) {
     .coffe svg{
       position: fixed;  
       bottom: 56px;
-      font-size: 40px;
+      color:#000;
     }
+    .coffe :hover{
+      color:rgba(0,0,0,0.5);
+    }
+  
     .share {
       background-image: linear-gradient(-45deg, #E0EA5E 0%, #30D158 100%);
       width: 64px;
@@ -196,9 +200,10 @@ function convertToHtml(nodes) {
     <h1 style="font-size: 80px;text-align: center; margin:4px;">🔖</h1>
     <div class="coffe">
       <a href="https://github.com/wexs/bookmarks">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" fill="currentColor"><path d="M12.001 2C6.47598 2 2.00098 6.475 2.00098 12C2.00098 16.425 4.86348 20.1625 8.83848 21.4875C9.33848 21.575 9.52598 21.275 9.52598 21.0125C9.52598 20.775 9.51348 19.9875 9.51348 19.15C7.00098 19.6125 6.35098 18.5375 6.15098 17.975C6.03848 17.6875 5.55098 16.8 5.12598 16.5625C4.77598 16.375 4.27598 15.9125 5.11348 15.9C5.90098 15.8875 6.46348 16.625 6.65098 16.925C7.55098 18.4375 8.98848 18.0125 9.56348 17.75C9.65098 17.1 9.91348 16.6625 10.201 16.4125C7.97598 16.1625 5.65098 15.3 5.65098 11.475C5.65098 10.3875 6.03848 9.4875 6.67598 8.7875C6.57598 8.5375 6.22598 7.5125 6.77598 6.1375C6.77598 6.1375 7.61348 5.875 9.52598 7.1625C10.326 6.9375 11.176 6.825 12.026 6.825C12.876 6.825 13.726 6.9375 14.526 7.1625C16.4385 5.8625 17.276 6.1375 17.276 6.1375C17.826 7.5125 17.476 8.5375 17.376 8.7875C18.0135 9.4875 18.401 10.375 18.401 11.475C18.401 15.3125 16.0635 16.1625 13.8385 16.4125C14.201 16.725 14.5135 17.325 14.5135 18.2625C14.5135 19.6 14.501 20.675 14.501 21.0125C14.501 21.275 14.6885 21.5875 15.1885 21.4875C19.259 20.1133 21.9999 16.2963 22.001 12C22.001 6.475 17.526 2 12.001 2Z"></path></svg>    </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" fill="currentColor"><path d="M12.001 2C6.47598 2 2.00098 6.475 2.00098 12C2.00098 16.425 4.86348 20.1625 8.83848 21.4875C9.33848 21.575 9.52598 21.275 9.52598 21.0125C9.52598 20.775 9.51348 19.9875 9.51348 19.15C7.00098 19.6125 6.35098 18.5375 6.15098 17.975C6.03848 17.6875 5.55098 16.8 5.12598 16.5625C4.77598 16.375 4.27598 15.9125 5.11348 15.9C5.90098 15.8875 6.46348 16.625 6.65098 16.925C7.55098 18.4375 8.98848 18.0125 9.56348 17.75C9.65098 17.1 9.91348 16.6625 10.201 16.4125C7.97598 16.1625 5.65098 15.3 5.65098 11.475C5.65098 10.3875 6.03848 9.4875 6.67598 8.7875C6.57598 8.5375 6.22598 7.5125 6.77598 6.1375C6.77598 6.1375 7.61348 5.875 9.52598 7.1625C10.326 6.9375 11.176 6.825 12.026 6.825C12.876 6.825 13.726 6.9375 14.526 7.1625C16.4385 5.8625 17.276 6.1375 17.276 6.1375C17.826 7.5125 17.476 8.5375 17.376 8.7875C18.0135 9.4875 18.401 10.375 18.401 11.475C18.401 15.3125 16.0635 16.1625 13.8385 16.4125C14.201 16.725 14.5135 17.325 14.5135 18.2625C14.5135 19.6 14.501 20.675 14.501 21.0125C14.501 21.275 14.6885 21.5875 15.1885 21.4875C19.259 20.1133 21.9999 16.2963 22.001 12C22.001 6.475 17.526 2 12.001 2Z"></path></svg>
       </a>
-      <div class="share" >
+    </div>
+    <div class="share" >
       <a href="https://song.bss.design/">🚧</a>
     </div>
     <div class="info">
@@ -207,80 +212,67 @@ function convertToHtml(nodes) {
     <h2>书签文件夹</h2>
     <ul>`;
 
-  function generateSidebar(nodes) {
-    nodes.forEach((node) => {
-      if (node.children && node.children.length > 0) {
-        html += `<li><a href="#folder-${node.id}">${node.title}</a></li>`;
-        node.children.forEach((child) => {
-          generateSidebar([child]); // Recursively add child folders to sidebar
-        });
-      }
-    });
-  }
-
-  generateSidebar(nodes);
-
-  html += `</ul></div><div class="content">`;
-
-  function generateBookmarkList(nodes, indent = 0) {
-    nodes.forEach((node) => {
-      if (node.children && node.children.length > 0) {
-        html += `${' '.repeat(indent)}<h2 id="folder-${node.id}" class="bookmark-title">${node.title}</h2>\n${' '.repeat(indent)}<ul>\n`;
-        node.children.forEach((child) => {
-          generateBookmarkList([child], indent + 2); // Recursively add child bookmarks
-        });
-        html += `${' '.repeat(indent)}</ul>\n`;
-      } else if (node.url) {
-        const domain = new URL(node.url).hostname.replace('www.', '');
-        const faviconUrl = `https://www.google.com/s2/favicons?sz=64&domain_url=${domain}`;
-        const displayTitle = node.title || node.url;
-        const color = stringToColor(domain);
-        const initial = domain.charAt(0).toUpperCase();
-        html += `${' '.repeat(indent)}<li class="link"><a href="${node.url}"><img src="${faviconUrl}" onerror="this.style.display='none';this.nextElementSibling.style.display='flex';"><div class="icon-placeholder" style="display: none; background-color:${color};">${initial}</div>${displayTitle}</a></li>\n`;
-      }
-    });
-  }
-
-  generateBookmarkList(nodes);
-
-  html += `
-  </div>
-</body>
-</html>`;
-
-  return html;
-}
-
-function stringToColor(str) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-  const color = (hash & 0x00FFFFFF)
-    .toString(16)
-    .toUpperCase();
-  return '#' + '00000'.substring(0, 6 - color.length) + color;
-}
-
-function formatHtml(html) {
-  const formatted = html.replace(/(>)(<)(\/*)/g, '$1\r\n$2$3');
-  let pad = 0;
-  return formatted.split('\r\n').map((node, index) => {
-    let indent = 0;
-    if (node.match(/.+<\/\w[^>]*>$/)) {
-      indent = 0;
-    } else if (node.match(/^<\/\w/)) {
-      if (pad !== 0) {
-        pad -= 1;
-      }
-    } else if (node.match(/^<\w[^>]*[^\/]>.*$/)) {
-      indent = 1;
-    } else {
-      indent = 0;
+    function generateSidebar(nodes) {
+      nodes.forEach((node) => {
+        if (node.children && node.children.length > 0) {
+          html += `<li><a href="#folder-${node.id}">${node.title}</a></li>`;
+          node.children.forEach((child) => {
+            generateSidebar([child]); // Recursively add child folders to sidebar
+          });
+        }
+      });
     }
-
-    const padding = '  '.repeat(pad);
-    pad += indent;
-    return padding + node;
-  }).join('\r\n');
-}
+  
+    generateSidebar(nodes);
+  
+    html += `</ul></div><div class="content">`;
+  
+    function generateBookmarkList(nodes, indent = 0) {
+      nodes.forEach((node) => {
+        if (node.children && node.children.length > 0) {
+          html += `${' '.repeat(indent)}<h2 id="folder-${node.id}" class="bookmark-title">${node.title}</h2>\n${' '.repeat(indent)}<ul>\n`;
+          node.children.forEach((child) => {
+            generateBookmarkList([child], indent + 2); // Recursively add child bookmarks
+          });
+          html += `${' '.repeat(indent)}</ul>\n`;
+        } else if (node.url) {
+          const domain = new URL(node.url).hostname.replace('www.', '');
+          const faviconUrl = node.url ? `https://www.google.com/s2/favicons?sz=64&domain_url=${domain}` : 'default-icon.png';
+          const displayTitle = node.title || node.url;
+          html += `${' '.repeat(indent)}<li class="link"><a href="${node.url}"><img src="${faviconUrl}" onerror="this.onerror=null;this.src='default-icon.png';" alt="Icon">${displayTitle}</a></li>\n`;
+        }
+      });
+    }
+  
+    generateBookmarkList(nodes);
+  
+    html += `
+    </div>
+  </body>
+  </html>`;
+  
+    return html;
+  }
+  
+  function formatHtml(html) {
+    const formatted = html.replace(/(>)(<)(\/*)/g, '$1\r\n$2$3');
+    let pad = 0;
+    return formatted.split('\r\n').map((node, index) => {
+      let indent = 0;
+      if (node.match(/.+<\/\w[^>]*>$/)) {
+        indent = 0;
+      } else if (node.match(/^<\/\w/)) {
+        if (pad !== 0) {
+          pad -= 1;
+        }
+      } else if (node.match(/^<\w[^>]*[^\/]>.*$/)) {
+        indent = 1;
+      } else {
+        indent = 0;
+      }
+  
+      const padding = '  '.repeat(pad);
+      pad += indent;
+      return padding + node;
+    }).join('\r\n');
+  }
