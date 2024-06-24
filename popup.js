b38d1eab9105d484849915b7c8c64d4291db85f3
wexs <wexstm@gmail.com>
2024-06-24 18:32:44 +0800

    列表过长 fix

--- popup.js
+++ popup.js
@@ -59,21 +59,30 @@ function convertToHtml(nodes) {
       min-width: 200px;
       background-image: linear-gradient(-45deg, #E0EA5E 0%, #30D158 100%);
       color: #000;
-      padding: 20px;
+      padding: 0px;
       box-sizing: border-box;
       height: 100%;
       overflow-y: auto;
+      z-index: 2;
+    }
+    .sidebar h1 {
+      text-align: center;
+      margin-top: 24px;
+      margin-bottom: 0px;
+      font-size: 80px;
     }
     .sidebar h2 {
       text-align: center;
-      margin: 0 0 20px;
+      margin: 0px;
       font-size: 24px;
     }
     .sidebar ul {
       list-style-type: none;
-      padding: 0;
-      margin-top: 40px;
+      padding: 20px;
+      padding-top: 0px;
+      padding-bottom: 80px;
       font-size: 16px;
+      margin-top: 0px;
     }
     .sidebar ul li {
       margin: 10px 0;
@@ -135,18 +144,24 @@ function convertToHtml(nodes) {
       padding-top:32px;
       margin-top: 0px;
     }
-    folder-0 {
-      user-select: none;
-    }
     .info p {
-      position: fixed;  
-      bottom: 12px;
       color: rgba(0,0,0,0.50);
       font-size: 14px;
+      margin: 0px;
+    }
+    .info {
+      bottom: 0px;
+      position: fixed;
+      background-color: #ffffff00;
+      backdrop-filter: saturate(180%) blur(20px);
+      -webkit-backdrop-filter: saturate(180%) blur(20px);;
+      width: 160px;
+      padding: 20px;
+      padding-top: 12px;
+      padding-bottom: 12px;
+      z-index: 999;
     }
     .coffe svg{
-      position: fixed;  
-      bottom: 56px;
       color:#000;
     }
     .coffe :hover{
@@ -197,16 +212,16 @@ function convertToHtml(nodes) {
 </head>
 <body>
   <div class="sidebar">
-    <h1 style="font-size: 80px;text-align: center; margin:4px;">🔖</h1>
-    <div class="coffe">
-      <a href="https://github.com/wexs/bookmarks">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" fill="currentColor"><path d="M12.001 2C6.47598 2 2.00098 6.475 2.00098 12C2.00098 16.425 4.86348 20.1625 8.83848 21.4875C9.33848 21.575 9.52598 21.275 9.52598 21.0125C9.52598 20.775 9.51348 19.9875 9.51348 19.15C7.00098 19.6125 6.35098 18.5375 6.15098 17.975C6.03848 17.6875 5.55098 16.8 5.12598 16.5625C4.77598 16.375 4.27598 15.9125 5.11348 15.9C5.90098 15.8875 6.46348 16.625 6.65098 16.925C7.55098 18.4375 8.98848 18.0125 9.56348 17.75C9.65098 17.1 9.91348 16.6625 10.201 16.4125C7.97598 16.1625 5.65098 15.3 5.65098 11.475C5.65098 10.3875 6.03848 9.4875 6.67598 8.7875C6.57598 8.5375 6.22598 7.5125 6.77598 6.1375C6.77598 6.1375 7.61348 5.875 9.52598 7.1625C10.326 6.9375 11.176 6.825 12.026 6.825C12.876 6.825 13.726 6.9375 14.526 7.1625C16.4385 5.8625 17.276 6.1375 17.276 6.1375C17.826 7.5125 17.476 8.5375 17.376 8.7875C18.0135 9.4875 18.401 10.375 18.401 11.475C18.401 15.3125 16.0635 16.1625 13.8385 16.4125C14.201 16.725 14.5135 17.325 14.5135 18.2625C14.5135 19.6 14.501 20.675 14.501 21.0125C14.501 21.275 14.6885 21.5875 15.1885 21.4875C19.259 20.1133 21.9999 16.2963 22.001 12C22.001 6.475 17.526 2 12.001 2Z"></path></svg>
-      </a>
-    </div>
+    <h1>🔖</h1>
     <div class="share" >
       <a href="https://song.bss.design/">🚧</a>
     </div>
     <div class="info">
+      <div class="coffe">
+        <a href="https://github.com/wexs/bookmarks">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" fill="currentColor"><path d="M12.001 2C6.47598 2 2.00098 6.475 2.00098 12C2.00098 16.425 4.86348 20.1625 8.83848 21.4875C9.33848 21.575 9.52598 21.275 9.52598 21.0125C9.52598 20.775 9.51348 19.9875 9.51348 19.15C7.00098 19.6125 6.35098 18.5375 6.15098 17.975C6.03848 17.6875 5.55098 16.8 5.12598 16.5625C4.77598 16.375 4.27598 15.9125 5.11348 15.9C5.90098 15.8875 6.46348 16.625 6.65098 16.925C7.55098 18.4375 8.98848 18.0125 9.56348 17.75C9.65098 17.1 9.91348 16.6625 10.201 16.4125C7.97598 16.1625 5.65098 15.3 5.65098 11.475C5.65098 10.3875 6.03848 9.4875 6.67598 8.7875C6.57598 8.5375 6.22598 7.5125 6.77598 6.1375C6.77598 6.1375 7.61348 5.875 9.52598 7.1625C10.326 6.9375 11.176 6.825 12.026 6.825C12.876 6.825 13.726 6.9375 14.526 7.1625C16.4385 5.8625 17.276 6.1375 17.276 6.1375C17.826 7.5125 17.476 8.5375 17.376 8.7875C18.0135 9.4875 18.401 10.375 18.401 11.475C18.401 15.3125 16.0635 16.1625 13.8385 16.4125C14.201 16.725 14.5135 17.325 14.5135 18.2625C14.5135 19.6 14.501 20.675 14.501 21.0125C14.501 21.275 14.6885 21.5875 15.1885 21.4875C19.259 20.1133 21.9999 16.2963 22.001 12C22.001 6.475 17.526 2 12.001 2Z"></path></svg>
+        </a>
+      </div>
       <p>© Maple design</p>
     </div>
     <h2>Bookmarks</h2>
